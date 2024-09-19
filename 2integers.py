3539955b3a1643980604dac14a7804be72ea59ea
divyasreedivya <tekkupallidivyasree@gmail.com>
2024-09-19 14:14:10 +0530

    Update 2integers.py

--- 2integers.py
+++ 2integers.py
@@ -6,9 +6,26 @@ The first input corresponds to  the first number.(a)
 The second input corresponds to the second number.(b)
 Output format:
 If the first number is equal to the second number, print "x and y are equal". Otherwise, print "x greater than y" or "x less than y"
+40
+20
+25
+Sample Output:
+L1
+
 '''
 Sample Input:
 6
 8
 Sample Output:
 6 less than 8
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+
+# Compare the two numbers and print the appropriate message
+if a == b:
+    print(f"{a} and {b} are equal.")
+elif a > b:
+    print(f"{a} is greater than {b}.")
+else:
+    print(f"{a} is less than {b}.")
+  
